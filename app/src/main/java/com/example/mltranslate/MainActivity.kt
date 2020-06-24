d25e8f9f0fc1f4948f5356c0321a48249156ef3b
Olaislord <rayjat51@gmail.com>
2020-06-24 03:49:10 +0100

    initail commit

---
+++ app/src/main/java/com/example/mltranslate/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mltranslate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mltranslate.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
